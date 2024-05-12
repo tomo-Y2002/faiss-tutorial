176b77202030d56ad1ef7fd4b2e9b8d0dd6c6426
tomo-Y2002 <tomohiro2014yamashita@gmail.com>
2024-05-12 14:59:18 +0900

    ivfに描画するコマンドを作成した。

--- ivf.py
+++ ivf.py
@@ -2,15 +2,16 @@ import faiss
 from data import xb, xq, d
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-nlists = np.logspace(1, 10000, 10)
+nlists = np.logspace(1, 3, 10).astype("int")
 time_train = []
 time_process = []
-for nlist in nlists:
+for idx, nlist in enumerate(nlists):
+  print(f"idx : {idx}, nlist : {nlist}--------")
   k = 4
   quantizer = faiss.IndexFlatL2(d)
-  index = faiss.IndexIVFFlat(quantizer, d, nlist)
+  index = faiss.IndexIVFFlat(quantizer, d, int(nlist))
   assert not index.is_trained
   start = time.time()
   index.train(xb)
@@ -24,3 +25,19 @@ for nlist in nlists:
   time_process.append(time.time()-start)
 
 # 描画
+plt.figure()
+plt.plot(nlists, time_train, label="train")
+plt.legend()
+plt.xlabel("nlist")
+plt.ylabel("train time [s]")
+plt.title("nlist - train_time")
+plt.savefig("data/ivf_nlist_train_time.png")
+plt.show()
+
+plt.figure()
+plt.plot(nlists, time_process, label="process")
+plt.xlabel("nlist")
+plt.ylabel("process time [s]")
+plt.title("nlist - process_time")
+plt.savefig("data/ivf_nlist_train_time.png")
+plt.show()
