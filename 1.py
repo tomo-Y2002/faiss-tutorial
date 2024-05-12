4b0dce93574debbbcf968099353017196f40d7da
tomo-Y2002 <tomohiro2014yamashita@gmail.com>
2024-05-12 15:35:22 +0900

    1.pyの線形探索のラッパーを作成

--- 1.py
+++ 1.py
@@ -1,18 +1,42 @@
 import faiss
 from data import xb, xq, d
-
-index = faiss.IndexFlatL2(d)
-print(index.is_trained)
-index.add(xb)
-print(index.ntotal)
-
 import time
-k = 4
-D, I = index.search(xb[:5], k)
-print(D)  # returns top k neighbor distance
-print(I)  # returns top k neighbor ID
-start = time.time()
-D, I = index.search(xq, k)
-print(f"time : {time.time() - start}")
+
+class FlatL2():
+  def __init__(self, k):
+    self.k = k
+
+  def train(self, xb, d):
+    """
+    returns time spend on training
+    """
+    self.index = faiss.IndexFlatL2(d)
+    self.index.add(xb)
+    time_train = 0
+    return time_train
+  
+  def search(self, xq):
+    """
+    returns 
+        D : distance nearest k neighbor (n, k)
+        I : Id nearest k neighbor (n, k)
+        time_search : time spend at searchings
+    """
+    start = time.time()
+    assert self.index.is_trained
+    D, I = self.index.search(xq, self.k)
+    time_search = time.time() - start
+    return D, I, time_search
+  
+obj = FlatL2(k = 4)
+time_train = obj.train(xb, d)
+D, I, _ = obj.search(xb[:5])
+print(D)
+print(I)
+_, I, time_search = obj.search(xq)
 print(I[:5])
 print(I[-5:])
+print(f"time_train : {time_train}, time_search : {time_search}")
+
+
+    
